8a975bbe8d3b376fa6747dc16c4e1de1f9262ae0
Justin Spencer <JustinSpencer1999@gmail.com>
2023-02-10 11:12:09 -0800

    added feature layers and adjusted starting position

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,36 @@
 const token = config.MY_API_TOKEN;
 
-require(["esri/config", "esri/Map", "esri/views/MapView"], function(esriConfig, Map, MapView) {
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/layers/GeoJSONLayer"], 
+function
+(esriConfig, Map, MapView, GeoJSONLayer) {
 
     esriConfig.apiKey = "MY_API_TOKEN";
 
+    //Create geojson layer for BC Floodplain layer
+
+    floodurl = "data/CWB_FLOODPLAINS_BC_AREA_SVW.geojson";
+
+    const floodlayer = new GeoJSONLayer ({
+      url: floodurl
+    });
+
+    //Create disaster response route geojson layer
+
+    disasterurl = "data/DRP_DISASTER_RESPNSE_ROUTES_SP.geojson";
+
+    const disasterlayer = new GeoJSONLayer ({
+      url: disasterurl
+    });
+
     const map = new Map({
-      basemap: "dark-gray" // Basemap layer service
+      basemap: "dark-gray", // Basemap layer service
+      layers: [floodlayer, disasterlayer]
     });
 
     const view = new MapView({
         map: map,
-        center: [-123.116226, 49.246292], // Longitude, latitude
-        zoom: 12, // Zoom level
+        center: [-122.911110, 49.206944], // Longitude, latitude
+        zoom: 11, // Zoom level
         container: "viewDiv" // Div element
     });
 });
