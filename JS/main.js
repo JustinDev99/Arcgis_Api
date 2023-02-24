ca10655df38e2156a828b5b58e6f86935d7240ed
Justin Spencer <JustinSpencer1999@gmail.com>
2023-02-24 11:31:59 -0800

    Bike Path Render

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,30 @@
 const token = config.MY_API_TOKEN;
 
-require(["esri/config", "esri/Map", "esri/views/MapView", "esri/renderers/UniqueValueRenderer", "esri/layers/GeoJSONLayer", "esri/widgets/Home"], 
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/renderers/Renderer", "esri/renderers/UniqueValueRenderer", "esri/layers/GeoJSONLayer", "esri/widgets/Home"], 
 function
-(esriConfig, Map, MapView, UniqueValueRenderer, GeoJSONLayer, Home) {
+(esriConfig, Map, MapView, Renderer, UniqueValueRenderer, GeoJSONLayer, Home) {
 
     esriConfig.apiKey = "MY_API_TOKEN";
+
+  //Create renderer for bike path layer
+
+  const bike_render = {
+    type: "simple",
+    symbol: {
+      color: "#BA55D3",
+      type: "simple-line",
+      style: "solid"
+    }
+  };  
   
   //Create simple fill Render for Borough Layer
 
   const colors = ["#d92b30", "#3cccb4", "#ffdf3c", "#c27c30", "#f260a1"];
 
   const boroughs = {
-    type: "simple-fill",
+    type: "simple-line",
     style: "solid",
+    width: "2px"
   };
 
   const statenIsland = {
@@ -76,7 +88,15 @@ function
     ]
   };
 
+  //Create NYC Bike Route Layer
 
+  BikeUrl = "data/New York City Bike Routes.geojson";
+
+  const bikelayer = new GeoJSONLayer ({
+    url: BikeUrl,
+    renderer: bike_render,
+    effect: "bloom(2, 0.1, 1)"
+  });
 
   //Create NYC Borough Layer
 
@@ -84,22 +104,14 @@ function
 
   const boundarylayer = new GeoJSONLayer ({
     url: boundaryurl,
-    opacity: "0.7",
+    opacity: "0.3",
     renderer: boro_render
   });
 
-  //Create NYC Bike Route Layer
-
-  BikeUrl = "data/New York City Bike Routes.geojson";
-
-  const bikelayer = new GeoJSONLayer ({
-    url: BikeUrl
+  const map = new Map({
+    basemap: "dark-gray", // Basemap layer service
+    layers: [bikelayer, boundarylayer]
   });
-
-    const map = new Map({
-      basemap: "dark-gray", // Basemap layer service
-      layers: [bikelayer, boundarylayer]
-    });
 
     const view = new MapView({
         map: map,
